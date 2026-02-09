46f876f3b1939a4d9314b0267d9a84e5c429888c
Leticia <0001159429@senaimgaluno.com.br>
2026-02-09 19:14:29 -0300

    Adicionando icone e função de ajuda no sistema

--- src/main/java/com/example/sistemaecomerceadidas/controller/MainController.java
+++ src/main/java/com/example/sistemaecomerceadidas/controller/MainController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 
 public class MainController {
 
@@ -21,6 +24,21 @@ public class MainController {
         MenuItem item = (MenuItem) event.getSource();
         String fxml = item.getUserData().toString();
         carregarTela(fxml);
+    }
+
+    @FXML
+    private void mostrarSobre() {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Sobre o Sistema");
+        alert.setHeaderText("Sistema Constance");
+
+        alert.setContentText(
+                "Versão: 1.0\n" +
+                        "Desenvolvido por: Leticia\n" +
+                        "Ano: 2026"
+        );
+
+        alert.showAndWait();
     }
 
     private void carregarTela(String fxml) {
